a6d1349d269fe15714be5c815626ca90e6ed01b0
suwatchai <k_suwatchai@hotmail.com>
2021-05-12 20:47:57 +0700

    Fix FirebaseJson tokens extraction issue.

--- src/ESP_Line_Notify.cpp
+++ src/ESP_Line_Notify.cpp
@@ -1,7 +1,7 @@
 /**
- * LINE Notify Arduino Library for ESP8266 and ESP32 version 1.0.5
+ * LINE Notify Arduino Library for ESP8266 and ESP32 version 1.0.6
  * 
- * April 4, 2021
+ * May 12, 2021
  *
  * This library provides ESP32 to perform REST API call to LINE Notify service to post the several message types.
  *
--- src/ESP_Line_Notify.h
+++ src/ESP_Line_Notify.h
@@ -1,6 +1,7 @@
 /**
- * LINE Notify Arduino Library for ESP8266 and ESP32 version 1.0.5
- * April 4, 2021
+ * LINE Notify Arduino Library for ESP8266 and ESP32 version 1.0.6
+ * 
+ * May 12, 2021
  *
  * This library provides ESP32 to perform REST API call to LINE Notify service to post the several message types.
  *
